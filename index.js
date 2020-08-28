59e2ab4e4483b601a8e98887dca4c46616850fe3
Eddie Cuellar <heriberto.cuellar1329@gmail.com>
2020-08-28 00:48:09 -0500

    Completed Functionality, need Memory Buttons

--- index.js
+++ index.js
@@ -1,54 +1,85 @@
-let memoryButtons = document.querySelectorAll('button.memoryButton');
-let numberButtons = document.querySelectorAll('button.numberButton');
-let operationButtons = document.querySelectorAll('button.operationButton');
-
+setListeners();
 let calculatorDisplay = document.getElementById('calculatorDisplay');
-calculatorDisplay.value = 0;
 
-let number1 = 0;
-let number2 = 0;
-let answer = 0;
+let number1 = null;
+let number2 = null;
+let answer = null;
+let memStorage = 0;
 
 let operation;
-let operationDirty = true;
+let init = true;
+let divByZero = false;
+let operationDirty = false;
 let equalDirty = false;
 
+function setListeners() {
+    let memoryButtons = document.querySelectorAll('button.memoryButton');
+    let numberButtons = document.querySelectorAll('button.numberButton');
+    let operationButtons = document.querySelectorAll('button.operationButton');
 
-for (i = 0; i < memoryButtons.length; i++) {
-    memoryButtons[i].addEventListener('click', function(e) { handleMemory(e); })
+    for (i = 0; i < memoryButtons.length; i++) {
+        memoryButtons[i].addEventListener('click', function(e) { handleMemory(e); })
+    }
+
+    for (i = 0; i < numberButtons.length; i++) {
+        numberButtons[i].addEventListener('click', function(e) { handleNumbers(e); })
+
+    }
+    for (i = 0; i < operationButtons.length; i++) {
+        operationButtons[i].addEventListener('click', function(e) { handleOperations(e); })
+    }
 }
 
-for (i = 0; i < numberButtons.length; i++) {
-    numberButtons[i].addEventListener('click', function(e) { handleNumbers(e); })
-
-}
-for (i = 0; i < operationButtons.length; i++) {
-    operationButtons[i].addEventListener('click', function(e) { handleOperations(e); })
-}
 
 function handleMemory(e) {
     console.log(e.target.innerText);
+    let memKey = e.target.innerText;
+    if (memKey === 'MR') {
+        calculatorDisplay.value = memStorage;
+        init = true;
+    } else if (memKey === 'M+') {
+        memStorage += parseFloat(calculatorDisplay.value);
+    } else {
+        memStorage -= parseFloat(calculatorDisplay.value);
+    }
 }
 
 function handleNumbers(e) {
     console.log(e.target.innerText);
+    console.log('initial = ' + init);
+    console.log('divByZero = ' + divByZero);
+    console.log('equal dirty = ' + equalDirty);
+    console.log('operation dirty = ' + operationDirty);
 
     if (e.target.innerText === 'C') {
         clearCalculator();
+        if (divByZero) {
+            divByZero = false;
+            setDisable(false);
+        }
         equalDirty = false;
     } else if (equalDirty) {
-        console.log('Reset After Equals')
         clearCalculator();
         calculatorDisplay.value = e.target.innerText;
-        equalDirty = false;
-        operationDirty = false;
+        init = false;
     } else {
-        if (operationDirty) {
+        if (operationDirty || init) {
             calculatorDisplay.value = '';
             operationDirty = false;
+            init = false;
         }
         calculatorDisplay.value += e.target.innerText;
     }
+}
+
+function clearCalculator() {
+    number1 = null;
+    number2 = null;
+    answer = null;
+    init = true;
+    operationDirty = false;
+    equalDirty = false;
+    calculatorDisplay.value = 0;
 }
 
 function handleOperations(e) {
@@ -66,9 +97,10 @@ function handleOperations(e) {
     console.log('answer : ' + answer);
 
     if (!equalDirty && !operationDirty) {
-        if (!number1 && !answer) {
+        console.log('INSIDE!!');
+        if (number1 === null && answer === null) {
             number1 = parseFloat(calculatorDisplay.value);
-        } else if (!number1 && answer) {
+        } else if (number1 === null && answer) {
             number1 = answer;
             number2 = parseFloat(calculatorDisplay.value);
             calculatorMath();
@@ -99,27 +131,33 @@ function calculatorMath() {
 
     if (operation === '+') {
         answer = number1 + number2;
-        number1 = 0;
-        number2 = 0;
+        number1 = null;
+        number2 = null;
         calculatorDisplay.value = answer;
         console.log('Addition');
     } else if (operation === '-') {
         answer = number1 - number2;
-        number1 = 0;
-        number2 = 0;
+        number1 = null;
+        number2 = null;
         calculatorDisplay.value = answer;
         console.log('Subtraction');
     } else if (operation === '*') {
         answer = number1 * number2;
-        number1 = 0;
-        number2 = 0;
+        number1 = null;
+        number2 = null;
         calculatorDisplay.value = answer;
         console.log('Multiplication');
     } else if (operation === '/') {
-        answer = number1 / number2;
-        number1 = 0;
-        number2 = 0;
-        calculatorDisplay.value = answer;
+        if (number2 === 0) {
+            divByZero = true;
+            setDisable(true);
+            calculatorDisplay.value = 'Cannot divide by zero';
+        } else {
+            answer = number1 / number2;
+            number1 = null;
+            number2 = null;
+            calculatorDisplay.value = answer;
+        }
         console.log('Division');
     }
 
@@ -129,9 +167,20 @@ function calculatorMath() {
     console.log('answer : ' + answer);
 }
 
-function clearCalculator() {
-    number1 = 0;
-    number2 = 0;
-    answer = 0;
-    calculatorDisplay.value = 0;
+function setDisable(bool) {
+    let memoryButtons = document.querySelectorAll('button.memoryButton');
+    let numberButtons = document.querySelectorAll('button.numberButton');
+    let operationButtons = document.querySelectorAll('button.operationButton');
+
+    for (i = 0; i < memoryButtons.length; i++) {
+        memoryButtons[i].disabled = bool;
+    }
+    for (i = 0; i < numberButtons.length; i++) {
+        if (numberButtons[i].innerText !== 'C') {
+            numberButtons[i].disabled = bool;
+        }
+    }
+    for (i = 0; i < operationButtons.length; i++) {
+        operationButtons[i].disabled = bool;
+    }
 }
