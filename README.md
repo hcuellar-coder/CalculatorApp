bfa7b986b6075bc62f867bc1195522e40ff92894
Eddie Cuellar <heriberto.cuellar1329@gmail.com>
2020-08-31 00:41:19 -0500

    Ready for review

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ A simple and clean calculator application written entirely in Vanilla JavaScript
 
 Code Located [Here](https://github.com/hcuellar-coder/CalculatorApp)
 
-Run Code [Here](https://codepen.io/hcuellar-coder/pen/abNwKaW)
+Run Code [Here](https://hcuellar-coder.github.io/CalculatorApp/)
 
 ![Calculator App](./images/calculatorApp.PNG)
 
@@ -11,4 +11,4 @@ Run Code [Here](https://codepen.io/hcuellar-coder/pen/abNwKaW)
 This calculator application has memory functionality and sleek design. It was a challenge to take into account all the different functionalities that we take for granted of the simple calculator. Standing on the shoulders of giants, I was able to use Vanilla Javascript, HTML and CSS to create a working calcualtor applicaiton. It also uses the local storage for its memory functionality. It was a fun challenge and enjoyed making it. 
 
 ## Author
-Heriberto Cuellar – Full Stack Software Developer - [LinkedIn](www.linkedin.com/in/heriberto-c-5aa11952)
+Heriberto Cuellar – Full Stack Software Developer - [LinkedIn](https://www.linkedin.com/in/heriberto-c-5aa11952)
--- index.js
+++ index.js
@@ -1,160 +1,137 @@
-let memoryButtons = document.querySelectorAll('button.memoryButton');
-let numberButtons = document.querySelectorAll('button.numberButton');
-let operationButtons = document.querySelectorAll('button.operationButton');
-let calculatorDisplay = document.getElementById('calculatorDisplay');
+let operation;
+let newNumberInput = true;
+let dividingByZero = false;
+let operationClicked = false;
+let equalClicked = false;
 
-setListeners();
+let calculatorDisplay = document.getElementById('calculator-display');
+
+document.querySelectorAll('button.memory-button').forEach(item => {
+    item.addEventListener('click', e => { handleMemoryButtons(e.target.innerText); });
+});
+
+document.querySelectorAll('button.number-button').forEach(item => {
+    item.addEventListener('click', e => { handleNumberButtons(e.target.innerText); });
+});
+
+document.querySelectorAll('button.operation-button').forEach(item => {
+    item.addEventListener('click', e => { handleOperations(e.target.innerText); });
+});
+
+document.querySelector('button.clear-button').addEventListener('click', handleClearButton);
+
+function handleMemoryButtons(e) {
+    let memoryButton = e;
+    let memoryRecall = parseFloat(window.localStorage.getItem('memoryRecall')) || 0;
+
+    if (memoryButton === 'MR') {
+        calculatorDisplay.value = memoryRecall;
+        newNumberInput = true;
+        operationClicked = false;
+        if (equalClicked) {
+            equalClicked = false;
+            answer = null;
+        }
+    } else if (memoryButton === 'M+') {
+        memoryRecall += parseFloat(calculatorDisplay.value);
+    } else {
+        memoryRecall -= parseFloat(calculatorDisplay.value);
+    }
+    window.localStorage.setItem('memoryRecall', memoryRecall);
+}
+
+function handleNumberButtons(e) {
+    let numberButton = e;
+
+    if (calculatorDisplay.value.includes('.')
+        && numberButton.includes('.')
+        && !operationClicked) {
+
+    } else if (equalClicked) {
+        resetGlobalVariables();
+        calculatorDisplay.value = numberButton;
+        newNumberInput = false;
+    } else {
+        if (operationClicked || newNumberInput) {
+            calculatorDisplay.value = '';
+            operationClicked = false;
+            newNumberInput = false;
+        }
+        calculatorDisplay.value += numberButton;
+    }
+}
+
+function handleClearButton() {
+    resetGlobalVariables();
+    if (dividingByZero) {
+        dividingByZero = false;
+        disableButtons(false);
+    }
+}
 
 let number1 = null;
 let number2 = null;
 let answer = null;
-let memStorage = 0;
-
-let operation;
-let newInput = true;
-let divByZero = false;
-let operationDirty = false;
-let equalDirty = false;
-
-if (window.localStorage.getItem('calculatorMem')) {
-    memStorage = parseFloat(window.localStorage.getItem('calculatorMem'));
-}
-
-function setListeners() {
-    for (i = 0; i < memoryButtons.length; i++) {
-        memoryButtons[i].addEventListener('click', function(e) { handleMemory(e.target.innerText); })
-    }
-
-    for (i = 0; i < numberButtons.length; i++) {
-        numberButtons[i].addEventListener('click', function(e) { handleNumbers(e.target.innerText); })
-
-    }
-    for (i = 0; i < operationButtons.length; i++) {
-        operationButtons[i].addEventListener('click', function(e) { handleOperations(e.target.innerText); })
-    }
-}
-
-
-function handleMemory(e) {
-    let memKey = e;
-    if (memKey === 'MR') {
-        calculatorDisplay.value = memStorage;
-        newInput = true;
-        operationDirty = false;
-        if (equalDirty) {
-            equalDirty = false;
-            answer = null;
-        }
-    } else if (memKey === 'M+') {
-        memStorage += parseFloat(calculatorDisplay.value);
-        window.localStorage.setItem('calculatorMem', memStorage);
-    } else {
-        memStorage -= parseFloat(calculatorDisplay.value);
-        window.localStorage.setItem('calculatorMem', memStorage);
-    }
-}
-
-function handleNumbers(e) {
-    let numberInput = e;
-
-    if (numberInput === 'C') {
-        clearCalculator();
-        if (divByZero) {
-            divByZero = false;
-            setDisable(false);
-        }
-        equalDirty = false;
-    } else if (equalDirty) {
-        clearCalculator();
-        calculatorDisplay.value = numberInput;
-        newInput = false;
-    } else {
-        if (operationDirty || newInput) {
-            calculatorDisplay.value = '';
-            operationDirty = false;
-            newInput = false;
-        }
-        calculatorDisplay.value += numberInput;
-    }
-}
 
 function handleOperations(e) {
-    let newOperation = e;
+    let operationButton = e;
 
-    if (!equalDirty && !operationDirty) {
+    if (!equalClicked && !operationClicked) {
         if (number1 === null && answer === null) {
             number1 = parseFloat(calculatorDisplay.value);
         } else if (number1 === null && answer) {
             number1 = answer;
             number2 = parseFloat(calculatorDisplay.value);
-            calculatorMath();
+            calculatorDisplay.value = (Math.round(calculateAnswer() * 1000) / 1000);
         } else {
             number2 = parseFloat(calculatorDisplay.value);
-            calculatorMath();
+            calculatorDisplay.value = (Math.round(calculateAnswer() * 1000) / 1000);
         }
     }
 
-    if (newOperation !== operation && newOperation !== '=') {
-        operation = newOperation;
-        equalDirty = false;
-    } else if (newOperation === '=') {
-        operation = newOperation;
-        equalDirty = true;
-    }
-
-    operationDirty = true;
+    operation = operationButton;
+    equalClicked = (operationButton === '=') ? true : false;
+    operationClicked = true;
 }
 
-function calculatorMath() {
+function calculateAnswer() {
     if (operation === '+') {
         answer = number1 + number2;
-        number1 = null;
-        number2 = null;
-        calculatorDisplay.value = answer;
     } else if (operation === '-') {
         answer = number1 - number2;
-        number1 = null;
-        number2 = null;
-        calculatorDisplay.value = answer;
     } else if (operation === '*') {
         answer = number1 * number2;
-        number1 = null;
-        number2 = null;
-        calculatorDisplay.value = answer;
     } else if (operation === '/') {
         if (number2 === 0) {
-            divByZero = true;
-            setDisable(true);
-            calculatorDisplay.value = 'Cannot divide by zero';
+            dividingByZero = true;
+            disableButtons(true);
         } else {
             answer = number1 / number2;
-            number1 = null;
-            number2 = null;
-            calculatorDisplay.value = answer;
         }
     }
+    number1 = null;
+    number2 = null;
+    return dividingByZero ? 'Cannot divide by zero' : answer;
 }
 
-function clearCalculator() {
+function resetGlobalVariables() {
     number1 = null;
     number2 = null;
     answer = null;
-    newInput = true;
-    operationDirty = false;
-    equalDirty = false;
+    newNumberInput = true;
+    operationClicked = false;
+    equalClicked = false;
     calculatorDisplay.value = 0;
 }
 
-function setDisable(bool) {
-    for (i = 0; i < memoryButtons.length; i++) {
-        memoryButtons[i].disabled = bool;
-    }
-    for (i = 0; i < numberButtons.length; i++) {
-        if (numberButtons[i].innerText !== 'C') {
-            numberButtons[i].disabled = bool;
-        }
-    }
-    for (i = 0; i < operationButtons.length; i++) {
-        operationButtons[i].disabled = bool;
-    }
+function disableButtons(bool) {
+    document.querySelectorAll('button.memory-button').forEach(item => {
+        item.disabled = bool;
+    });
+    document.querySelectorAll('button.number-button').forEach(item => {
+        item.disabled = bool;
+    });
+    document.querySelectorAll('button.operation-button').forEach(item => {
+        item.disabled = bool;
+    });
 }
